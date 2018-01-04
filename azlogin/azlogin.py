93ca5b8730f2634a987342b79ad3bff172fe3e75
Frode Hus <frodehus@outlook.com>
2018-01-04 14:56:53 +0100

    implemented simple token cache and token refresh

--- azlogin/azlogin.py
+++ azlogin/azlogin.py
@@ -6,7 +6,10 @@ import pathlib
 import subprocess
 from os import getenv
 
+CACHEFILE = ".tokencache"
+
 class TokenHelper(object):
+
     def __init__(self, client_id, app_id, authority):
         self._app_id = app_id
         self._client_id = client_id
@@ -16,29 +19,55 @@ class TokenHelper(object):
 
     def _load_cache(self):
         cache = adal.TokenCache()
-        file = pathlib.Path(".tokencache")
+        file = pathlib.Path(CACHEFILE)
         if not file.is_file():
             return cache
-        data = file.read_text()
-        cache.deserialize(data)
+        with file.open() as data:
+            cache.deserialize(data.read())
         return cache
 
+    def _save_cache(self):
+        if not self._cache:
+            return
+
+        cached = self._cache.serialize()
+        with open(CACHEFILE, "w") as out:
+            out.write(cached)
+
+    def get_token(self, user):
+        token = None
+        if user:
+            print("Requesting token for {} using cache...".format(user))
+            token = self._context.acquire_token(self._app_id, user, self._client_id)
+            if token is None:
+                print("User not found or token expired...")
+                token = self.get_token_using_device_code()
+        else:
+            token = self.get_token_using_device_code()
+        self._save_cache()
+        return token
+
     def get_token_using_device_code(self):
-        code = self._context.acquire_user_code(self._app_id, "28700222-ef32-4a52-a003-5e0ddc134b26")
+        code = self._context.acquire_user_code(self._app_id, self._client_id)
         print(code['message'])
         token = self._context.acquire_token_with_device_code(self._app_id, code, self._client_id)
         return token
 
 def main():
-    user = getenv("KUBECTL_PLUGINS_LOCAL_FLAG_USER")
     tenantId = getenv("KUBECTL_PLUGINS_LOCAL_FLAG_TENANT")
     appId = getenv("KUBECTL_PLUGINS_LOCAL_FLAG_APPID")
     clientId = getenv("KUBECTL_PLUGINS_LOCAL_FLAG_CLIENTID")
+    user = getenv("KUBECTL_PLUGINS_LOCAL_FLAG_USER", None)
 
     helper = TokenHelper(clientId, appId, "https://login.microsoftonline.com/" + tenantId)
-    token = helper.get_token_using_device_code()
+    token = helper.get_token(user)
+    if token is None:
+        print("Failed to retrieve token")
+        sys.exit(1)
+
     accessToken = token["accessToken"]
-    subprocess.run(["kubectl", "config", "set-credentials", user, "--token", accessToken])
+    userId = token["userId"]
+    subprocess.call(["kubectl", "config", "set-credentials", userId, "--token", accessToken])
 
 if __name__ == "__main__":
     main()
